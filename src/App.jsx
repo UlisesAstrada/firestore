77aec9b76158b3321dc4c302c7b1d94bb5a565c6
Ulises <ulises_astrada@outlook.com>
2021-03-04 20:05:13 -0300

    Form added to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="col">
+          <h2>Formulario de usuarios</h2>
+          <form className="form-group">
+            <input 
+              className="form-control"
+              type="text"
+              placeholder="Introduce el nombre"
+            />
+            <input
+              className="form-control mt-3"
+              type="text"
+              placeholder="Introduce el nombre"
+            />
+            <input type="submit" value="Registrar" className="btn btn-dark btn-block"/>
+          </form>
+        </div>
+        <div className="col">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
