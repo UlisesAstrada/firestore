e349e3ace078930103f2fb5e4b3dd9b7564c768a
Ulises <ulises_astrada@outlook.com>
2021-03-04 22:24:50 -0300

    useState used

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const[nombre, setNombre] = useState('')
+  const[phone, setPhone] = useState('')
+  const[usuario, setUsuario] = useState([])
+
+  const setUsuarios = (e) => {
+    e.preventDefault()
+    if(!nombre.trim()) 
+  }
+
   return (
     <div className="App">
       <div className="row">
         <div className="col">
           <h2>Formulario de usuarios</h2>
-          <form className="form-group">
+          <form className="form-group ml-3">
             <input 
+              onChange={(e) =>{setNombre(e.target.value)}}
               className="form-control"
               type="text"
               placeholder="Introduce el nombre"
             />
             <input
+              onChange={(e) =>{setPhone(e.target.value)}}
               className="form-control mt-3"
               type="text"
-              placeholder="Introduce el nombre"
+              placeholder="Introduce el número"
             />
-            <input type="submit" value="Registrar" className="btn btn-dark btn- mt-3"/>
+            <input  type="submit" value="Registrar" className="btn btn-dark btn- mt-3"/>
           </form>
         </div>
         <div className="col">
-
+          <h2>Lista de tu agenda</h2>
         </div>
       </div>
     </div>
